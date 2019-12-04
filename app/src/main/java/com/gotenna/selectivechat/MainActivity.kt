29b1b3c7352777c807868ae3373cd3ebebe6c537
ketaki03 <ketaki@gotenna.com>
2019-12-04 16:41:59 -0500

    User login

--- app/src/main/java/com/gotenna/selectivechat/MainActivity.kt
+++ app/src/main/java/com/gotenna/selectivechat/MainActivity.kt
@@ -1,29 +1,75 @@
 package com.gotenna.selectivechat
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        setUI()
+
+        setUpFirebase()
+    }
+
+    private fun getUserList(): List<String> {
+
+        val database = FirebaseDatabase.getInstance().reference
+
+        val userMutableList = mutableListOf<String>()
+        userMutableList.add(0,"Select User")
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (a in dataSnapshot.children) {
+                    userMutableList.add(a.child("name").value.toString())
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        }
+
+        database.child("Hack_day").child("members").addValueEventListener(postListener)
+
+        return userMutableList
+    }
+
+    private fun setUI() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
 
-        setUpFirebase()
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item, getUserList()
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        user_spinner.adapter = adapter
+        user_spinner.setSelection(0)
+        user_spinner.onItemSelectedListener = this
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -42,7 +88,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun setUpFirebase(){
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        //user_spinner.prompt = "Please select user from list"
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        with (sharedPref.edit()) {
+            putString("user", getUserList()[p2])
+            commit()
+        }
+
+        //go to your chat fragment
+    }
+
+    fun setUpFirebase() {
         FirebaseInstanceId.getInstance().instanceId
             .addOnCompleteListener(OnCompleteListener { task ->
                 if (!task.isSuccessful) {
